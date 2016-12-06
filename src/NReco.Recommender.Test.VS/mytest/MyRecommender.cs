d5892a37c31daab640aa5923412a5657cf24dd7f
lcksy <lcksy+110>
2016-12-06 21:57:47 +0800

    2016-12-6

---
+++ src/NReco.Recommender.Test.VS/mytest/MyRecommender.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NReco.CF.Taste.Impl.Model.File;
+using NReco.CF.Taste.Impl.Neighborhood;
+using NReco.CF.Taste.Impl.Recommender;
+using NReco.CF.Taste.Impl.Similarity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NReco.Recommender.Test.VS.mytest
+{
+    [TestClass]
+    public class MyRecommender
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var model = new FileDataModel("data.csv");
+            var similarity = new LogLikelihoodSimilarity(model);
+            var neighborhood = new NearestNUserNeighborhood(3, similarity, model);
+            var recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+            var recommendedItems = recommender.Recommend(1, 5);
+        }
+    }
+}
