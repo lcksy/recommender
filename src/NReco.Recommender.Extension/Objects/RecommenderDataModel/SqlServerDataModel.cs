f658573fc54e600f8dd49e889cfbd316669f02fb
lcksy <421047431@qq.com>
2016-12-16 16:39:47 +0800

    2016-12-16

---
+++ src/NReco.Recommender.Extension/Objects/RecommenderDataModel/SqlServerDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NReco.CF.Taste.Common;
+using NReco.CF.Taste.Impl.Common;
+using NReco.CF.Taste.Impl.Model;
+using NReco.CF.Taste.Model;
+
+namespace NReco.Recommender.Extension.Objects.RecommenderDataModel
+{
+    public class SqlServerDataModel
+    {
+    
+    }
+}
